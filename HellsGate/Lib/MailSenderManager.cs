f9cc6914eeebe76640ff0a7fddc8172aa0f8c2c2
matteo <garato.omicron@gmail.com>
2018-11-25 11:58:56 +0100

    [ADD] - Garato: first mail attempt

---
+++ HellsGate/Lib/MailSenderManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace HellsGate.Lib
+{
+    public class MailSenderManager
+    {
+        private SmtpClient client = new SmtpClient();
+        private MailMessage mailMessage = new MailMessage();
+
+        public MailSenderManager()
+        {
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("username", "password");
+        }
+        public bool SendMessage()
+        {
+            try
+            {
+                mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress("whoever@me.com");
+                mailMessage.To.Add("receiver@me.com");
+                mailMessage.Body = "body";
+                mailMessage.Subject = "subject";
+                client.Send(mailMessage);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
