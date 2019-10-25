5630daa9c321207087cedf4e2d82901000a7a4e4
MatteoGarato <matteo.garato@madlab.biz>
2019-10-25 17:23:39 +0200

    [ADD] - View

---
+++ HellsGate/Lib/LoginManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace HellsGate.Lib
+{
+    public static class LoginManager
+    {
+
+        public static async Task<bool> VerifyLogin(string user, string CyphredPassword) =>
+            //https://code.msdn.microsoft.com/Token-Based-Authentication-6db2acc9#content
+            //TODO: create a token based authentication for the mainteneance page
+            false;
+    }
+}
