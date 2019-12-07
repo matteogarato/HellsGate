e711194b3e139954963ba9e25e6bd29e065733fe
matteo <garato.omicron@gmail.com>
2019-12-07 21:16:27 +0100

    [ADD] - xUnitTest

---
+++ XUnitTest/TestWebApi/CardVerificationTest.cs
@@ -0,0 +1,20 @@
+﻿using HellsGate.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTest.TestWebApi
+{
+    public class CardVerificationTest
+    {
+        CardVerificationController _test;
+        [Theory]
+        [InlineData("AA123BB")]
+        public void AdminCard(string value)
+        {
+            Assert.True(_test.Get(value).Result);
+        }
+
+    }
+}
