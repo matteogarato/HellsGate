ba7e77a191632ae549c4556317abb06c22cec66b
matteo <garato.omicron@gmail.com>
2020-03-01 11:56:17 +0100

    [ADD] - reformat

--- HellsGate/Pages/Areas/Identity/Account/Logout.cshtml.cs
+++ HellsGate/Pages/Areas/Identity/Account/Logout.cshtml.cs
@@ -1,7 +1,6 @@
 using HellsGate.Lib;
 using HellsGate.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
--- HellsGate/Services/IMenuService.cs
+++ HellsGate/Services/IMenuService.cs
@@ -1,8 +1,5 @@
 ﻿using HellsGate.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HellsGate.Services
 {
