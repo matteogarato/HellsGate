bbc8cff61711335dbfb902b3d84b997d3e69e8fe
matteo <garato.omicron@gmail.com>
2020-04-18 22:51:14 +0200

    [ADD] - node and migration

--- HellsGate/Migrations/20200418204853_Dev004.Designer.cs
+++ HellsGate/Migrations/20200418204853_Dev004.Designer.cs
@@ -4,14 +4,16 @@ using HellsGate.Models.DatabaseModel;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HellsGate.Migrations
 {
     [DbContext(typeof(HellsGateContext))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20200418204853_Dev004")]
+    partial class Dev004
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
