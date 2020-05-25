3e277589f7e473ee53d24ec2cce1fe34b605aae2
matteo <garato.omicron@gmail.com>
2020-05-25 08:42:13 +0200

    [ADD] - reading from rfid

--- HellsGate.Node/HellsGate.Node.py
+++ HellsGate.Node/HellsGate.Node.py
@@ -1,9 +1,10 @@
 import configparser
-import requests
-import RPi.GPIO as GPIO
+#import requests
+#import RPi.GPIO as GPIO
 from uuid import getnode as get_mac
 import time
 
+configParser = configparser.RawConfigParser()
 configFilePath = r'HellsGateNode.Config'
 
 def main():
@@ -15,8 +16,8 @@ def main():
      RELAIS_1_GPIO = int(configParser.get('API', 'PinOut'))
      openTime = int(configParser.get('API', 'OpenTime'))
      nodeName = configParser.get('API', 'nodeName')
-     GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
-     GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
+     #GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
+     #GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
      auth = {'Authorization': 'token {}'.format(secreToken)}
      mac = get_mac()
      with open('/dev/tty0', 'r') as tty:
@@ -24,14 +25,18 @@ def main():
              RFID_input = tty.readline().rstrip()
              if(RFID_input):
                  #todo: authentication call
-                 nodeData = '{"cardNumber": "{}",  "macAddress": "{}",  "nodeName": "{}"}'.format(RFID_input,mac,nodeName)
-                 if (requests.get(url, auth=auth, data=json.dumps(nodeData))):
-                      print('Open')
-                      GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
-                      time.sleep(openTime)
-                      GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
-                 else:
-                     GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+                 #nodeData = '{"cardNumber": "{}",  "macAddress": "{}",  "nodeName": "{}"}'.format(RFID_input,mac,nodeName)
+                 print(RFID_input)
+                 #if (requests.get(url, auth=auth, data=json.dumps(nodeData))):
+                 #     print('Open')
+                 #     GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
+                 #     time.sleep(openTime)
+                 #     GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+                 #else:
+                 #    GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
     except Exception as e:
                 print(e)
                 main()
+
+if __name__ == '__main__':
+    main()
