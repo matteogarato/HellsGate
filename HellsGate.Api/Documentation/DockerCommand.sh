5494859f272948fbbd87a3d283762c56c2b93330
matteo <garato.omicron@gmail.com>
2020-06-14 11:31:31 +0200

    [FIX] - fix DockerCommand

--- HellsGate.Api/Documentation/DockerCommand.sh
+++ HellsGate.Api/Documentation/DockerCommand.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 sudo git pull
 #DockerImageCreate
-sudo docker stop hellsgate-image
+sudo docker rm $(docker stop $(docker ps -a -q --filter ancestor=<hellsgate-image> --format="{{.ID}}"))
 sudo docker build -t hellsgate-image -f Dockerfile .
 sudo docker create --name hellsgate hellsgate-image
 sudo docker save hellsgate-image -o hellsgate.tar
 
 #Docker load & start
-sudo docker load -i hellsgate.tar
+#sudo docker load -i hellsgate.tar
 sudo docker run -d -p 80:80 hellsgate-image
