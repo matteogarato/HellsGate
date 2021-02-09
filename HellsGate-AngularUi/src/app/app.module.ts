ae3c9a5f843f380de5ad595d9dc6fe313e2bc89d
matteo <matteo.garato92@gmail.com>
2021-02-09 18:15:35 +0100

    [ADD] - Angular login dev

--- HellsGate-AngularUi/src/app/app.module.ts
+++ HellsGate-AngularUi/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
@@ -11,7 +12,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
